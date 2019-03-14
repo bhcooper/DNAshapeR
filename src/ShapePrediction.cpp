7c64c6206e4d593a2f39b2bb363e69f435789ea6
bhcooper <bhcooper@ucdavis.edu>
2019-03-13 18:55:06 -0700

    Update ShapePrediction.cpp

--- src/ShapePrediction.cpp
+++ src/ShapePrediction.cpp
@@ -102,11 +102,11 @@ std::vector<std::vector<std::string>> getDNAShape(std::string fastaFilePath, std
 		if (line[0] != '>')
 		{
 		    allOut.push_back(split(line, ','));
-		    for (auto &val : line)
+		    /*for (auto &val : line)
 		    {
 			std::cout << val << " ";
-		    }
-		    std::cout << '\n';
+		    }*/
+		    std::cout << line << '\n';
 		}
 	}
 	      
