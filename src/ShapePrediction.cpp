a90708fade61ef924b86068473a9d64aa4818630
bhcooper <bhcooper@ucdavis.edu>
2019-03-13 19:12:19 -0700

    Update ShapePrediction.cpp

--- src/ShapePrediction.cpp
+++ src/ShapePrediction.cpp
@@ -12,14 +12,15 @@ using namespace Rcpp;
 
 std::vector<std::string> split(const std::string& s, char delimiter)
 {
-   std::cout << s << '\n';
    std::vector<std::string> tokens;
    std::string token;
    std::istringstream tokenStream(s);
    while (std::getline(tokenStream, token, delimiter))
    {
       tokens.push_back(token);
+      std::cout << token << " "
    }
+   stc::cout << '\n'
    return tokens;
 }
 
