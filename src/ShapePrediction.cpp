b010f17a9458babac5745dbcbd0a53810d5791dd
bhcooper <bhcooper@ucdavis.edu>
2019-03-13 19:27:10 -0700

    Update ShapePrediction.cpp

--- src/ShapePrediction.cpp
+++ src/ShapePrediction.cpp
@@ -23,7 +23,7 @@ std::vector<std::string> split(const std::string& s, char delimiter)
 }
 
 // [[Rcpp::export]]
-Rcpp:wrap<std::vector<std::vector<std::string>>> getDNAShape(std::string fastaFilePath, std::string shapeType){
+Rcpp::wrap<std::vector<std::vector<std::string>>> getDNAShape(std::string fastaFilePath, std::string shapeType){
   std::ifstream in_fstream(fastaFilePath.c_str());
   std::string outputFile;
   std::vector<std::vector<std::string>> allOut;
