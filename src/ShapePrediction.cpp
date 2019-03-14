6d4a5bccb67d5cef8e7e7fb8f4f27cd1bbd72013
bhcooper <bhcooper@ucdavis.edu>
2019-03-13 19:43:17 -0700

    Update ShapePrediction.cpp

--- src/ShapePrediction.cpp
+++ src/ShapePrediction.cpp
@@ -201,14 +201,6 @@ std::vector<std::vector<std::string>> getDNAShape(std::string fastaFilePath, std
         Rcout << "Cannot recogize the shape type(MGW/Roll/HelT/ProT/EP):" << shapeType << std::endl;
       }
   }
-  for(auto &line : allOut)
-  {
-    for(auto &val : line)
-    {
-      std::cout << val << " ";
-    }
-    std::cout << '\n';
-  }
   return allOut;
 }
 
